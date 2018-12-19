e7a1e1a97a20495e2206aa12569694b38d5c48fc
Štefan Tomášik <TomasikS>
2018-12-19 21:37:19 +0100

    project in school

---
+++ Database.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+ 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Database
+    {
+        private string server = "localhost";
+        private string user = "root";
+        private string password = "root";
+        private string port = "3306";
+        private string database = "bankomat";
+        private MySqlConnection conn;
+
+
+        public void openConnection()
+        {
+            string conStr = "server=" + server +
+                      ";user=" + user +
+                      ";database=" + database +
+                      ";port=" + port +
+                      ";password=" + password + ";";
+            conn = new MySqlConnection(conStr);
+
+            if (conn == null) Console.WriteLine("error");
+            else Console.WriteLine("ok");
+        }
+
+        internal int checkPIN(string code)
+        {
+             
+            string q = "SELECT counter FROM cards WHERE pin LIKE"+ code;
+
+            MySqlCommand com = new MySqlCommand(q);
+            MySqlDataReader r = com.ExecuteReader();
+            if (r.Read())
+            {
+                Console.WriteLine("ok");
+
+            }
+            return 0;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        Form f=new Form();
+        string code;
+        int count;
+
+        public Form1()
+        {
+            InitializeComponent();
+            code = "";
+            count = 0;
+        }
+
+ 
+        private void processclick(int v) {
+
+
+            if (count < 4)
+            {
+                string str = v.ToString();
+                code = code + str;
+                textBox1.Text = code;
+                count++;
+               
+              
+                button12.Enabled = (textBox1.Text.Length ==4);
+                   
+                
+            }
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            processclick(1);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            processclick(2);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            processclick(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            processclick(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            processclick(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            processclick(6);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            processclick(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            processclick(8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            processclick(9);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            count =0; code = " ";textBox1.Text = " ";textBox1.Enabled = true;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            processclick(0);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            Database d = new Database();
+            d.openConnection();
+            //0 ok 1 -zly  2 - blokovana karta
+            int res = d.checkPIN(code);
+
+        }
+    }
+}
